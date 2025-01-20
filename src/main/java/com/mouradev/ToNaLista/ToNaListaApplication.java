2ca8742ff74c07ff4caa866d315731944ad0f6cf
gmourarpn <gustavo.moura@gruporpn.com.br>
2025-01-20 16:19:41 -0300

    first commit

---
+++ src/main/java/com/mouradev/ToNaLista/ToNaListaApplication.java
@@ -0,0 +1,13 @@
+package com.mouradev.ToNaLista;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToNaListaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToNaListaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mouradev/ToNaLista/ToNaListaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mouradev.ToNaLista;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToNaListaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
